a2323ecb780ed5d7b1ea29d3a9a46e1442f88752
Jon Eyolfson <jon@eyolfson.com>
2014-01-21 10:45:43 -0500

    Improve performance of gitolite triggers
    On a small system of 30 repos and users, this is the runtime before:
    
        $ time gitolite trigger POST_COMPILE
        real    1m7.612s
        user    0m49.530s
        sys     0m4.647s
    
    This is the runtime after:
    
        $ time gitolite trigger POST_COMPILE
        real    0m1.382s
        user    0m0.633s
        sys     0m0.060s
    
    However, this code should be rewritten more clearly.

--- eyl/django/gitolite/management/commands/gitolitetrigger.py
+++ eyl/django/gitolite/management/commands/gitolitetrigger.py
@@ -3,7 +3,7 @@
 # You should have received a copy of the GNU General Public License along with
 # this program. If not, see <http://www.gnu.org/licenses/>.
 
-from subprocess import call, check_output, DEVNULL
+from subprocess import check_output, Popen, DEVNULL, PIPE
 
 from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand, CommandError
@@ -27,23 +27,73 @@ class Command(BaseCommand):
                 return
             self.post_create(*args)
 
-    def sync(self, path):
+    def repo_clean(self, path):
         repo, created = Repo.objects.get_or_create(path)
         # Ensure the repo is synced with gitolite
         if not created:
             repo.sync()
             repo.save()
         Access.objects.filter(repo=repo).delete()
-        for user in get_user_model().objects.all():
-            username = user.username
-            if not call(['gitolite', 'access', '-q', path, username, 'R']):
-                Access.objects.create(repo=repo, user=user)
+        return repo
 
     def post_compile(self):
         output = check_output(['gitolite', 'list-phy-repos'], stderr=DEVNULL)
-        repo_paths = [x.decode('utf-8') for x in output.splitlines()]
-        for path in repo_paths:
-            self.sync(path)
+        repo_paths = [x.decode() for x in output.splitlines()]
+        repo_list = [self.repo_clean(x) for x in repo_paths]
+        user_list = list(get_user_model().objects.all())
+        with Popen(['gitolite', 'access', '%', '%', 'R', 'any'],
+                   stdin=PIPE, stdout=PIPE, stderr=DEVNULL,
+                   universal_newlines=True) as p:
+
+            for repo in repo_list:
+                for user in user_list:
+                    p.stdin.write(repo.path)
+                    p.stdin.write(' ')
+                    p.stdin.write(user.username)
+                    p.stdin.write('\n')
+            p.stdin.close()
+
+            access_list = []
+            i = repo_list.__iter__()
+            j = user_list.__iter__()
+            repo = next(i)
+            for line in p.stdout:
+                ret = line.strip().split('\t')[2]
+                try:
+                    user = next(j)
+                except StopIteration:
+                    j = user_list.__iter__()
+                    repo = next(i)
+                    user = next(j)
+                if not ret.startswith('DENIED'):
+                    access_list.append(Access(repo=repo, user=user))
+                if len(access_list) > 100:
+                    Access.objects.bulk_create(access_list)
+                    access_list = []
+            if len(access_list) != 0:
+                Access.objects.bulk_create(access_list)
 
     def post_create(self, path, username, operation):
-        self.sync(path)
+        repo = self.repo_clean(path)
+        user_list = list(get_user_model().objects.all())
+        with Popen(['gitolite', 'access', repo.path, '%', 'R', 'any'],
+                   stdin=PIPE, stdout=PIPE, stderr=DEVNULL,
+                   universal_newlines=True) as p:
+
+            for user in user_list:
+                p.stdin.write(user.username)
+                p.stdin.write('\n')
+            p.stdin.close()
+
+            access_list = []
+            j = user_list.__iter__()
+            for line in p.stdout:
+                ret = line.strip().split('\t')[2]
+                user = next(j)
+                if not ret.startswith('DENIED'):
+                    access_list.append(Access(repo=repo, user=user))
+                if len(access_list) > 100:
+                    Access.objects.bulk_create(access_list)
+                    access_list = []
+            if len(access_list) != 0:
+                Access.objects.bulk_create(access_list)
