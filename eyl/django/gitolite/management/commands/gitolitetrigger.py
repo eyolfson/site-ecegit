6d2e3b943b84f58cfc0ca85da09dfaf8d527e8b8
Jon Eyolfson <jon@eyolfson.com>
2014-01-22 15:09:13 -0500

    Rearranged gitolite trigger due to buffers being too large

--- eyl/django/gitolite/management/commands/gitolitetrigger.py
+++ eyl/django/gitolite/management/commands/gitolitetrigger.py
@@ -3,6 +3,8 @@
 # You should have received a copy of the GNU General Public License along with
 # this program. If not, see <http://www.gnu.org/licenses/>.
 
+import io
+
 from subprocess import check_output, Popen, DEVNULL, PIPE
 
 from django.contrib.auth import get_user_model
@@ -27,69 +29,27 @@ class Command(BaseCommand):
                 return
             self.post_create(*args)
 
-    def repo_clean(self, path):
+    def sync(self, path, user_list=list(get_user_model().objects.all())):
         repo, created = Repo.objects.get_or_create(path)
         # Ensure the repo is synced with gitolite
         if not created:
             repo.sync()
             repo.save()
         Access.objects.filter(repo=repo).delete()
-        return repo
-
-    def post_compile(self):
-        output = check_output(['gitolite', 'list-phy-repos'], stderr=DEVNULL)
-        repo_paths = [x.decode() for x in output.splitlines()]
-        repo_list = [self.repo_clean(x) for x in repo_paths]
-        user_list = list(get_user_model().objects.all())
-        with Popen(['gitolite', 'access', '%', '%', 'R', 'any'],
-                   stdin=PIPE, stdout=PIPE, stderr=DEVNULL,
-                   universal_newlines=True) as p:
-
-            for repo in repo_list:
-                for user in user_list:
-                    p.stdin.write(repo.path)
-                    p.stdin.write(' ')
-                    p.stdin.write(user.username)
-                    p.stdin.write('\n')
-            p.stdin.close()
-
-            access_list = []
-            i = repo_list.__iter__()
-            j = user_list.__iter__()
-            repo = next(i)
-            for line in p.stdout:
-                ret = line.strip().split('\t')[2]
-                try:
-                    user = next(j)
-                except StopIteration:
-                    j = user_list.__iter__()
-                    repo = next(i)
-                    user = next(j)
-                if not ret.startswith('DENIED'):
-                    access_list.append(Access(repo=repo, user=user))
-                if len(access_list) > 100:
-                    Access.objects.bulk_create(access_list)
-                    access_list = []
-            if len(access_list) != 0:
-                Access.objects.bulk_create(access_list)
-
-    def post_create(self, path, username, operation):
-        repo = self.repo_clean(path)
-        user_list = list(get_user_model().objects.all())
         with Popen(['gitolite', 'access', repo.path, '%', 'R', 'any'],
                    stdin=PIPE, stdout=PIPE, stderr=DEVNULL,
                    universal_newlines=True) as p:
-
+            buf = io.StringIO()
             for user in user_list:
-                p.stdin.write(user.username)
-                p.stdin.write('\n')
-            p.stdin.close()
-
+                buf.write(user.username)
+                buf.write('\n')
+            out, err = p.communicate(buf.getvalue())
+            buf.close()
             access_list = []
-            j = user_list.__iter__()
-            for line in p.stdout:
+            user_iter = user_list.__iter__()
+            for line in out.splitlines():
                 ret = line.strip().split('\t')[2]
-                user = next(j)
+                user = next(user_iter)
                 if not ret.startswith('DENIED'):
                     access_list.append(Access(repo=repo, user=user))
                 if len(access_list) > 100:
@@ -97,3 +57,14 @@ class Command(BaseCommand):
                     access_list = []
             if len(access_list) != 0:
                 Access.objects.bulk_create(access_list)
+
+    def post_compile(self):
+        output = check_output(['gitolite', 'list-phy-repos'], stderr=DEVNULL,
+                              universal_newlines=True)
+        repo_paths = output.splitlines()
+        user_list = list(get_user_model().objects.all())
+        for path in repo_paths:
+            self.sync(path, user_list)
+
+    def post_create(self, path, username, operation):
+        self.sync(path)
