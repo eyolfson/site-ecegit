926c161c9535fd5e4e6819e291a91b378d383999
Jon Eyolfson <jon@eyl.io>
2014-03-06 13:17:03 -0500

    Fixed typo

--- emailer/models.py
+++ emailer/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-from django_gitolite import Repo
+from django_gitolite.models import Repo
 
 class Notification(models.Model):
     user = models.ForeignKey(User, related_name="notifications")
