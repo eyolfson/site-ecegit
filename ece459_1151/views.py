7b0aa70b36d44cf253ed6b32ff490782920a4f73
Jon Eyolfson <jon@eyl.io>
2015-03-02 11:11:18 -0500

    Removed explicit staff for ece459_1151

--- ece459_1151/views.py
+++ ece459_1151/views.py
@@ -51,7 +51,6 @@ def assignment(request, slug):
             gitolite_creator_call('fork ece459/1151/a3 {}'.format(r))
             gitolite_creator_call('perms {} + WRITERS {}'.format(r, username))
             gitolite_creator_call('perms {} + WRITERS {}'.format(r, partner.username))
-            gitolite_creator_call('perms {} + WRITERS @ece459-1151-staff'.format(r))
             gitolite_creator_call('perms {} + READERS ece459@costa.cs.uwaterloo.ca'.format(r))
             try:
                 repo = Repo.objects.get(path=r)
@@ -66,7 +65,6 @@ def assignment(request, slug):
             r = 'ece459/1151/a3/g{}'.format(str(g.pk))
             gitolite_creator_call('fork ece459/1151/a3 {}'.format(r))
             gitolite_creator_call('perms {} + WRITERS {}'.format(r, username))
-            gitolite_creator_call('perms {} + WRITERS @ece459-1151-staff'.format(r))
             gitolite_creator_call('perms {} + READERS ece459@costa.cs.uwaterloo.ca'.format(r))
             try:
                 repo = Repo.objects.get(path=r)
